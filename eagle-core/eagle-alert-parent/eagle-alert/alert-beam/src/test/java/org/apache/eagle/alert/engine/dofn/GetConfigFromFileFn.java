5780caae315a155596828ba2de9afeefc47707d4
tangjijun <tangjijun@yhd.com>
2017-06-14 16:37:26 +0800

    add timestamp for config object

--- eagle-core/eagle-alert-parent/eagle-alert/alert-beam/src/test/java/org/apache/eagle/alert/engine/dofn/GetConfigFromFileFn.java
+++ eagle-core/eagle-alert-parent/eagle-alert/alert-beam/src/test/java/org/apache/eagle/alert/engine/dofn/GetConfigFromFileFn.java
@@ -9,10 +9,8 @@ import org.apache.eagle.alert.engine.coordinator.StreamDefinition;
 import org.apache.eagle.alert.engine.coordinator.StreamPartition;
 import org.apache.eagle.alert.engine.coordinator.StreamSortSpec;
 import org.apache.eagle.alert.engine.factory.SpecFactory;
-import org.apache.eagle.alert.engine.publisher.PublishConstants;
+import org.joda.time.Instant;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -30,7 +28,7 @@ public class GetConfigFromFileFn extends DoFn<KV<String, String>, SpoutSpec> {
     public GetConfigFromFileFn(TupleTag<SpoutSpec> spoutSpecTupleTag, TupleTag<Map<String, StreamDefinition>> sdsTag,
                                TupleTag<List<StreamPartition>> spTag, TupleTag<RouterSpec> routerSpecTupleTag,
                                TupleTag<PublishSpec> publishSpecTupleTag, TupleTag<Map<StreamPartition, StreamSortSpec>> sssTag,
-                               TupleTag<Map<StreamPartition, List<StreamRouterSpec>>> srsTag,TupleTag<AlertBoltSpec> alertBoltSpecTupleTag
+                               TupleTag<Map<StreamPartition, List<StreamRouterSpec>>> srsTag, TupleTag<AlertBoltSpec> alertBoltSpecTupleTag
     ) {
         this.spoutSpecTupleTag = spoutSpecTupleTag;
         this.sdsTag = sdsTag;
@@ -45,16 +43,16 @@ public class GetConfigFromFileFn extends DoFn<KV<String, String>, SpoutSpec> {
 
     @ProcessElement
     public void processElement(ProcessContext c) {
-        c.output(spoutSpecTupleTag, SpecFactory.createSpoutSpec());
-        c.output(sdsTag, SpecFactory.createSds());
+        c.outputWithTimestamp(spoutSpecTupleTag, SpecFactory.createSpoutSpec(), new Instant(System.currentTimeMillis()));
+        c.outputWithTimestamp(sdsTag, SpecFactory.createSds(), new Instant(System.currentTimeMillis()));
         List<StreamPartition> sps = Lists.newArrayList(SpecFactory.createRouterSpec().makeSSS().keySet());
-        c.output(spTag, sps);
+        c.outputWithTimestamp(spTag, sps, new Instant(System.currentTimeMillis()));
         RouterSpec routerSpec = SpecFactory.createRouterSpec();
-        c.output(routerSpecTupleTag, routerSpec);
-        c.output(sssTag, routerSpec.makeSSS());
-        c.output(srsTag, routerSpec.makeSRS());
-        c.output(publishSpecTupleTag, SpecFactory.createPublishSpec());
-        c.output(alertBoltSpecTupleTag, SpecFactory.createAlertSpec());
+        c.outputWithTimestamp(routerSpecTupleTag, routerSpec, new Instant(System.currentTimeMillis()));
+        c.outputWithTimestamp(sssTag, routerSpec.makeSSS(), new Instant(System.currentTimeMillis()));
+        c.outputWithTimestamp(srsTag, routerSpec.makeSRS(), new Instant(System.currentTimeMillis()));
+        c.outputWithTimestamp(publishSpecTupleTag, SpecFactory.createPublishSpec(), new Instant(System.currentTimeMillis()));
+        c.outputWithTimestamp(alertBoltSpecTupleTag, SpecFactory.createAlertSpec(), new Instant(System.currentTimeMillis()));
 
     }
 }

c7a824fe4f214ae944f1e2f37fb063455e40e829
tangjijun <tangjijun@yhd.com>
2017-06-14 14:39:57 +0800

    fix checkstyle problem

--- eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/evaluator/impl/AlertStreamCallbackBeam.java
+++ eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/evaluator/impl/AlertStreamCallbackBeam.java
@@ -13,43 +13,44 @@ import java.util.List;
 
 public class AlertStreamCallbackBeam extends StreamCallback {
 
-  private static final Logger LOG = LoggerFactory.getLogger(AlertStreamCallbackBeam.class);
-  private final PolicyDefinition policyDefinition;
-  private final StreamDefinition streamDefinition;
-  private final StreamPartition sp;
-  private final String outputStream;
-  private final List<AlertStreamEvent> results;
+    private static final Logger LOG = LoggerFactory.getLogger(AlertStreamCallbackBeam.class);
+    private final PolicyDefinition policyDefinition;
+    private final StreamDefinition streamDefinition;
+    private final StreamPartition sp;
+    private final String outputStream;
+    private final List<AlertStreamEvent> results;
 
-  public AlertStreamCallbackBeam(PolicyDefinition policyDefinition,
-      StreamDefinition streamDefinition, String outputStream, StreamPartition sp,List<AlertStreamEvent> results) {
-    this.policyDefinition = policyDefinition;
-    this.streamDefinition = streamDefinition;
-    this.outputStream = outputStream;
-    this.sp = sp;
-    this.results = results;
-  }
-
-  @Override public void receive(Event[] events) {
-    String policyName = policyDefinition.getName();
-    String siteId = policyDefinition.getSiteId();
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Generated {} alerts from policy '{}'", events.length, policyName);
+    public AlertStreamCallbackBeam(PolicyDefinition policyDefinition,
+                                   StreamDefinition streamDefinition, String outputStream, StreamPartition sp, List<AlertStreamEvent> results) {
+        this.policyDefinition = policyDefinition;
+        this.streamDefinition = streamDefinition;
+        this.outputStream = outputStream;
+        this.sp = sp;
+        this.results = results;
     }
-    for (Event e : events) {
-      AlertStreamEvent event = new AlertStreamEvent();
-      event.setSiteId(siteId);
-      event.setTimestamp(e.getTimestamp());
-      event.setData(e.getData());
-      event.setStreamId(outputStream);
-      event.setPolicyId(policyName);
-      event.setCreatedBy("Beam engine "+ sp.toString() + siteId);
-      event.setCreatedTime(System.currentTimeMillis());
-      event.setSchema(streamDefinition);
 
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("Generate new alert event: {}", event);
-      }
-      results.add(event);
+    @Override
+    public void receive(Event[] events) {
+        String policyName = policyDefinition.getName();
+        String siteId = policyDefinition.getSiteId();
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Generated {} alerts from policy '{}'", events.length, policyName);
+        }
+        for (Event e : events) {
+            AlertStreamEvent event = new AlertStreamEvent();
+            event.setSiteId(siteId);
+            event.setTimestamp(e.getTimestamp());
+            event.setData(e.getData());
+            event.setStreamId(outputStream);
+            event.setPolicyId(policyName);
+            event.setCreatedBy("Beam engine " + sp.toString() + siteId);
+            event.setCreatedTime(System.currentTimeMillis());
+            event.setSchema(streamDefinition);
+
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Generate new alert event: {}", event);
+            }
+            results.add(event);
+        }
     }
-  }
 }
